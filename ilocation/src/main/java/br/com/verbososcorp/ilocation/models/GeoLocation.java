4b81b2910f750419a912db2113ada6f6896d20e9
Rogério Fidnecio <rogerio.fidencio.dev@gmail.com>
2022-03-22 02:45:44 -0300

    create models geolocation and order

---
+++ ilocation/src/main/java/br/com/verbososcorp/ilocation/models/GeoLocation.java
@@ -0,0 +1,38 @@
+package br.com.verbososcorp.ilocation.models;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "geolocation")
+public class GeoLocation {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "location_id", nullable = false)
+	private Integer id;
+	
+	@Column(name = "timastamp", nullable = false)
+	private Timestamp timestamp;
+	
+	@Column(name = "latitude", nullable = false)
+	private String latitude;
+	
+	@Column(name = "longitude", nullable = false)
+	private String longitude;
+	
+	@ManyToOne
+	@JoinColumn(name = "order_id")
+	@JsonIgnoreProperties("geoLocationGroup")
+	private Order order;
+}
---
+++ ilocation/src/main/java/br/com/verbososcorp/ilocation/models/Order.java
@@ -0,0 +1,34 @@
+package br.com.verbososcorp.ilocation.models;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "order")
+public class Order {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "order_id", nullable = false)
+	private Integer id;
+	
+	@Column(name = "order_date", nullable = false)
+	private LocalDateTime date;
+	
+	@Column(name = "order_status", nullable = false)
+	private Integer status;
+	
+	@OneToMany(mappedBy = "order")
+	@JsonIgnoreProperties("order")
+	List<GeoLocation> geoLocationGroup;
+}
