1ba8003ce55db6f37e3cef79e7e1ba8463a729f0
Henrique <henrique1.7mu@hotmail.com>
2022-03-22 16:22:22 -0300

    feat/Validation,ValidationExcep,RegisterDeliveryPerson

--- ilocation/src/main/java/br/com/verbososcorp/ilocation/services/Impl/DeliveryPersonServiceImpl.java
+++ ilocation/src/main/java/br/com/verbososcorp/ilocation/services/Impl/DeliveryPersonServiceImpl.java
@@ -1,12 +1,58 @@
 package br.com.verbososcorp.ilocation.services.Impl;
 
 import br.com.verbososcorp.ilocation.DAO.DeliveryPersonDAO;
+import br.com.verbososcorp.ilocation.models.Customer;
+import br.com.verbososcorp.ilocation.models.DeliveryPerson;
 import br.com.verbososcorp.ilocation.services.interfaces.DeliveryPersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
 
-public class DeliveryPersonServiceImpl implements DeliveryPersonService {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+
+@Component
+@Primary
+@Qualifier("default")
+public class DeliveryPersonServiceImpl implements DeliveryPersonService, UserDetailsService {
 
     @Autowired
-    DeliveryPersonDAO deliveryPersonDAO;
+    private DeliveryPersonDAO dao;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<DeliveryPerson> userOP = dao.findByEmail(email);
+
+        if(userOP.isEmpty()){
+            throw new UsernameNotFoundException("Dados incorretos");
+        }
+
+        DeliveryPerson user = userOP.get();
+
+        //aqui pegar as roles/permitions
+
+        return new User(user.getEmail(), user.getPassword(), new ArrayList<>()); // permitions iriam no array vazio
+    }
+
+
+    @Override
+    public ResponseEntity<DeliveryPerson> register(DeliveryPerson newDeliveryPerson) {
+        newDeliveryPerson.setPassword(passwordEncoder.encode(newDeliveryPerson.getPassword()));
+
+        dao.save(newDeliveryPerson);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newDeliveryPerson);
+    }
 }
