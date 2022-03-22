2837d3cbe579efbffd8194c3d78a702ab4b6425d
Rogério Fidnecio <rogerio.fidencio.dev@gmail.com>
2022-03-22 03:15:51 -0300

    correção de nomes dos campos da tabela customer

--- ilocation/src/main/java/br/com/verbososcorp/ilocation/models/Customer.java
+++ ilocation/src/main/java/br/com/verbososcorp/ilocation/models/Customer.java
@@ -7,36 +7,36 @@ import java.util.List;
 
 
 @Entity
-@Table(name = "cliente")
+@Table(name = "customer")
 public class Customer {
 
     @Id
-    @Column(name = "costumer_id")
+    @Column(name = "customer_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(name = "costumer_name", nullable = false, length = 50)
+    @Column(name = "customer_name", nullable = false, length = 50)
     private String name;
 
-    @Column(name = "costumer_cpf", nullable = false, length = 11)
+    @Column(name = "customer_cpf", nullable = false, length = 11)
     private String cpf;
 
-    @Column(name = "costumer_email", nullable = false, length = 100)
+    @Column(name = "customer_email", nullable = false, length = 100)
     private String email;
 
-    @Column(name = "costumer_phone", nullable = false, length = 11)
+    @Column(name = "customer_phone", nullable = false, length = 11)
     private String phone;
 
-    @Column(name = "costumer_cep", nullable = false, length = 9)
+    @Column(name = "customer_cep", nullable = false, length = 9)
     private String cep;
 
-    @Column(name = "costumer_num_res", nullable = false, length = 10)
+    @Column(name = "customer_num_res", nullable = false, length = 10)
     private String numRes;
 
-    @Column(name = "costumer_compl", length = 30)
+    @Column(name = "customer_compl", length = 30)
     private String compl;
 
-    @OneToMany(mappedBy = "costumer")
-    @JsonIgnoreProperties("costumer")
+    @OneToMany(mappedBy = "customer")
+    @JsonIgnoreProperties("customer")
     List<String> OrderList; // mudar para Order
 }
