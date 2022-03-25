2e30ce7c5403e16c104ca97768d89f94709508a7
amaral.carolina <w.anapereira@gmail.com>
2022-03-25 14:28:40 -0300

    funcao getCurrentOrderByDeliveryPersonId, OrderDTO e outros

--- ilocation/src/main/java/br/com/verbososcorp/ilocation/DAO/OrderDAO.java
+++ ilocation/src/main/java/br/com/verbososcorp/ilocation/DAO/OrderDAO.java
@@ -1,11 +1,21 @@
 package br.com.verbososcorp.ilocation.DAO;
 
-import org.springframework.data.repository.CrudRepository;
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import br.com.verbososcorp.ilocation.DTO.OrderDTO;
 import br.com.verbososcorp.ilocation.models.Order;
 
 public interface OrderDAO extends CrudRepository<Order, Integer> {
 	
-
+	@Query("select new br.com.verbososcorp.ilocation.DTO.OrderDTO(order.id, order.status, order.deliveryPerson.id) FROM Order as order WHERE order.status = 1 AND order.deliveryPerson.id = :id")
+	public Optional<OrderDTO> getCurrentOrderByDeliveryPersonId(@Param("id") Integer id);
+	
+	
+	public List<Order> getByStatus(Integer status);
 
 }
