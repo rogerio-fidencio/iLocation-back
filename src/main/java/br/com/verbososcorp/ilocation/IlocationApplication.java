7381544ae3f2d7b8f8b8df4a3351e9131bb1b5ba
Rogério Fidnecio <rogerio.fidencio.dev@gmail.com>
2022-03-28 20:07:59 -0300

    feature swagger

--- src/main/java/br/com/verbososcorp/ilocation/IlocationApplication.java
+++ src/main/java/br/com/verbososcorp/ilocation/IlocationApplication.java
@@ -2,7 +2,6 @@ package br.com.verbososcorp.ilocation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.context.annotation.Bean;
--- src/main/java/br/com/verbososcorp/ilocation/security/SecurityConfiguration.java
+++ src/main/java/br/com/verbososcorp/ilocation/security/SecurityConfiguration.java
@@ -2,9 +2,8 @@ package br.com.verbososcorp.ilocation.security;
 
 
 
-import br.com.verbososcorp.ilocation.Filters.CustomAuthenticationFilter;
-import br.com.verbososcorp.ilocation.Filters.CustomAuthorizationFilter;
-import br.com.verbososcorp.ilocation.services.interfaces.DeliveryPersonService;
+import static br.com.verbososcorp.ilocation.util.Project.BASE_URL;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,13 +11,16 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import static br.com.verbososcorp.ilocation.util.Project.BASE_URL;
+import br.com.verbososcorp.ilocation.Filters.CustomAuthenticationFilter;
+import br.com.verbososcorp.ilocation.Filters.CustomAuthorizationFilter;
+import br.com.verbososcorp.ilocation.services.interfaces.DeliveryPersonService;
 
 @Configuration
 @EnableWebSecurity
@@ -55,4 +57,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
+    
+    @Override
+	public void configure(final WebSecurity webSecurity) {
+		webSecurity.ignoring().antMatchers(
+				 	"/v2/api-docs", 
+		            "/swagger-resources/**",  
+		            "/swagger-ui.html", 
+		            "/webjars/**" ,
+		             /*Probably not needed*/ "/swagger.json");
+	}
+
 }
