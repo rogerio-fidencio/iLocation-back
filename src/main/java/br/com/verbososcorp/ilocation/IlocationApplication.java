1e5879a6aa888a5b6b757e89951c11fc4de92467
Rogério Fidnecio <rogerio.fidencio.dev@gmail.com>
2022-03-29 09:14:53 -0300

    fix ilocationAppliccation

--- src/main/java/br/com/verbososcorp/ilocation/IlocationApplication.java
+++ src/main/java/br/com/verbososcorp/ilocation/IlocationApplication.java
@@ -2,9 +2,9 @@ package br.com.verbososcorp.ilocation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class IlocationApplication {
