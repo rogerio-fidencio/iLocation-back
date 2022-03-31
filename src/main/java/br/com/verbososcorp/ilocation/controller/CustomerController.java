3d254edae720c976e6bccca6f7dad61fb5d8870a
amaral.carolina <w.anapereira@gmail.com>
2022-03-30 21:16:45 -0300

    adding crossOrigin on Controllers

--- src/main/java/br/com/verbososcorp/ilocation/controller/CustomerController.java
+++ src/main/java/br/com/verbososcorp/ilocation/controller/CustomerController.java
@@ -1,19 +1,22 @@
 package br.com.verbososcorp.ilocation.controller;
 
-import br.com.verbososcorp.ilocation.exceptions.customExceptions.InternalServerErrorException;
-import br.com.verbososcorp.ilocation.models.Customer;
-import br.com.verbososcorp.ilocation.services.interfaces.CustomerService;
+import static br.com.verbososcorp.ilocation.util.Project.BASE_URL;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
-import static br.com.verbososcorp.ilocation.util.Project.BASE_URL;
+import br.com.verbososcorp.ilocation.exceptions.customExceptions.InternalServerErrorException;
+import br.com.verbososcorp.ilocation.models.Customer;
+import br.com.verbososcorp.ilocation.services.interfaces.CustomerService;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping(BASE_URL + "/customer")
 public class CustomerController {
 
